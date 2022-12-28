19b30169a43fb1bd5a1c5c2e6236a2fc19d34f6f
Mahary R <mrafanomezana@bocasay.com>
2022-12-28 09:29:33 +0300

    Creating fetch function

---
+++ api/fetchFunctions.ts
@@ -0,0 +1,9 @@
+export const basicFetch = async <returnType>(endpoint: string): Promise<returnType> => {
+    const response = await fetch(endpoint);
+    if(!response.ok){
+        throw new Error("Error fetching data!")
+    }
+    const data = await response.json();
+
+    return data;
+}
